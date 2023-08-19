b40b3a26f4b48bfc5fba210a8a8a549b729d9733
dvoe4ka <dvoooe4ka@yandex.ru>
2023-08-19 13:08:37 +0300

    fix2

--- stats-service/stats-client/src/main/java/org/example/client/BaseClient.java
+++ stats-service/stats-client/src/main/java/org/example/client/BaseClient.java
@@ -31,7 +31,7 @@ public class BaseClient {
         return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
     }
 
-    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String, Object> parameters, @Nullable T body) {
+    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, Map<String, Object> parameters, @Nullable T body) {
         HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
 
         ResponseEntity<Object> statsSvc;
