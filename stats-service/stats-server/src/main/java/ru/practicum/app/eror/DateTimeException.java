940ba96e5afa2414926b9367851ce1a0a8cb2581
dvoe4ka <dvoooe4ka@yandex.ru>
2023-09-01 05:45:37 +0300

    checkstyle

--- stats-service/stats-server/src/main/java/ru/practicum/app/eror/DateTimeException.java
+++ stats-service/stats-server/src/main/java/ru/practicum/app/eror/DateTimeException.java
@@ -1,6 +1,6 @@
 package ru.practicum.app.eror;
 
-public class DateTimeException extends RuntimeException{
+public class DateTimeException extends RuntimeException {
     public DateTimeException(String message) {
         super(message);
     }
